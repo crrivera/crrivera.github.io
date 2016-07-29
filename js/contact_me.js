a8d8917cacb697e94f380965b18a084e87498484
“Carlos <carlos.rivera.ece@gmail.com>
2016-07-29 14:45:50 -0700

    fix email form

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//formspree.io/carlos.rivera.ece@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
